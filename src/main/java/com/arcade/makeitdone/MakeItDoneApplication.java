96076565bc0f0b9283974ac5d909d2115f80a958
alireza <theethanwardhere@gmail.com>
2026-02-12 23:16:48 +0330

    Project base structure

---
+++ src/main/java/com/arcade/makeitdone/MakeItDoneApplication.java
@@ -0,0 +1,11 @@
+package com.arcade.makeitdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeItDoneApplication {
+    static void main(String[] args) {
+        SpringApplication.run(MakeItDoneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/arcade/makeitdone/MakeItDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.makeitdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeItDoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
